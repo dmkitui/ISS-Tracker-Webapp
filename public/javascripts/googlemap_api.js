e129b1fdf13b6e4b9f7e96a602a715ca6eececc9
Daniel Masake Kitui <daniel.kitui@andela.com>
2018-10-23 21:20:21 +0300

    Add panning map to center the ISS position after zooming in or out

--- public/javascripts/googlemap_api.js
+++ public/javascripts/googlemap_api.js
@@ -1,10 +1,24 @@
 var map;
+
 async function initMap() {
 	// Get ISS position and center map on the same.
 	let position = await getPosition();
-	console.log('XXXX: ', position)
+	let currentPosition = {lat: parseInt(position.latitude), lng: parseInt(position.longitude)};
 	map = new google.maps.Map(document.getElementById('map-div'), {
-		center: {lat: parseInt(position.latitude), lng: parseInt(position.longitude)},
-		zoom: 7
+		center: currentPosition,
+		zoom: 3
 	});
+	let marker = new google.maps.Marker({
+		position: currentPosition,
+		map: map,
+		icon: "images/iss.png",
+		animation: google.maps.Animation.DROP,
+		label: {text: `Latitude ${position.latitude}, Longitude: ${position.longitude}`, color: "white"}
+	});
+  	map.addListener('center_changed', function() {
+		// 3 seconds after the center of the map has changed, pan back to the marker.
+		window.setTimeout(function() {
+		  map.panTo(marker.getPosition());
+		}, 3000);
+  	});
 }
