a122ebba45d2d5d388181baea6cad13b8f843c1b
Daniel Masake Kitui <daniel.kitui@andela.com>
2018-10-23 21:21:40 +0300

    Add functionality to fetch iss position data

--- public/javascripts/iss_position.js
+++ public/javascripts/iss_position.js
@@ -1,21 +1,9 @@
-async function getPosition () {
-	console.log('Hey......am  here')
-	// return (await fetch('http://api.open-notify.org/iss-now.json')
-	// 	.then(res => res.json())
-	// 	.then((data) => {
-	// 		console.log(data.iss_position)
-	// 		if(data.message && data.message === "success") {
-	// 			return data.iss_position;
-	// 		}
-	// 	}).catch(err => console.error(err))
-	// );
-    let response = await fetch('http://api.open-notify.org/iss-now.json')
+async function getPosition() {
+    let response = await fetch('http://api.open-notify.org/iss-now.json');
 	if (response.ok) {
-		data = await response.json();
-		console.log("DAta: ", data)
+		let data = await response.json();
 		return data.iss_position;
 	} else {
-		throw new Error(response.status)
+		throw new Error(response.status);
 	}
 }
-
