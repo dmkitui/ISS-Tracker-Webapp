047bcffeb5536248d7730e7aa03cdba8b1213ade
Daniel Masake Kitui <daniel.kitui@andela.com>
2018-10-23 14:13:17 +0300

    Get google map api key from environment and pass it to map pug file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
+require('dotenv').config();
+
+const key = process.env.GOOGLE_API_KEY;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('index', {title: 'ISS Tracker webapp'});
+	res.render('index', {title: 'ISS Tracker', google_api_key: key});
 });
 
 module.exports = router;
