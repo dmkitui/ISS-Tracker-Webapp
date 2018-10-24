0138a2e1ee59ba98e97d92391799407653696da5
Daniel Masake Kitui <daniel.kitui@andela.com>
2018-10-24 19:23:54 +0300

    Remove unused file

--- public/javascripts/iss_position.js
+++
@@ -1,9 +0,0 @@
-async function getPosition() {
-    let response = await fetch('http://api.open-notify.org/iss-now.json');
-	if (response.ok) {
-		let data = await response.json();
-		return data.iss_position;
-	} else {
-		throw new Error(response.status);
-	}
-}
