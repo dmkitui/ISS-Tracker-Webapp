d031671e88733b4cb3b18503a57358aa7b94cf91
Daniel Masake Kitui <daniel.kitui@andela.com>
2018-10-25 17:43:52 +0300

    Add mongoose, bodyparser and session

--- app.js
+++ app.js
@@ -3,13 +3,35 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const session = require('express-session');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const homeRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
 
-// view engine setup
+app.use(session({
+	secret:'sdfsdfdsfdsfds3243223dsfsdf2332423erwe',
+	resave: true,
+	saveUninitialized: false
+}));
+
+mongoose.connect('mongodb://localhost:27017/iss-tracker', { useNewUrlParser: true });
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'db error'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -41,3 +63,5 @@ app.use(function(err, req, res, next) {
 
 app.listen(3000);
 console.log('To-do API server running at: http://localhost:3000');
+
+module.exports.db = db;
