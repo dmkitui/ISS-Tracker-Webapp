aa7e2f1a634bddf8699a5796859ad8ce9f1eefe7
Daniel Masake Kitui <daniel.kitui@andela.com>
2018-10-25 17:45:50 +0300

    Add signup GET and POST routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const userController = require("../controllers/userController");
+
+router.get("/signup", function(req, res, next) {
+  res.render("users/signup", {title: "ISS Tracker - Sign Up"});
 });
+
+router.post("/signup", function(req, res, next) {
+  console.log("Posting a users stuff....", req.body)
+  userController.signupUser(req, res);
+});
+
 
 module.exports = router;
