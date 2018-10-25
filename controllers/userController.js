01c2a8ea31bcac34ea792e77e697152372d221dd
Daniel Masake Kitui <daniel.kitui@andela.com>
2018-10-25 18:19:22 +0300

    Add user login functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,4 +16,23 @@ const signupUser = function(req, res){
 	});
 };
 
+const loginUser = function(req, res, next) {
+	if(req.body.email && req.body.password) {
+		User.authenticate(req.body.email, req.body.password, function(error, user) {
+			if (error || !user) {
+				const error = new Error('Wrong email or password');
+				error.status = 401;
+				return next(error);
+			} else {
+				req.session.userId = user._id;
+				res.redirect('/home');
+			}
+		});
+	} else {
+		const error = new Error('Username and/or password is required')
+		return next(error);
+	}
+}
+
+module.exports.loginUser = loginUser;
 module.exports.signupUser = signupUser;
