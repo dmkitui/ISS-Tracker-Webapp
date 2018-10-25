ad64b658c4a217a146404f8ab3550bfca946ca18
Daniel Masake Kitui <daniel.kitui@andela.com>
2018-10-25 18:21:26 +0300

    Add login POST and GET routes

--- routes/users.js
+++ routes/users.js
@@ -12,5 +12,14 @@ router.post("/signup", function(req, res, next) {
   userController.signupUser(req, res);
 });
 
+router.get("/login", function(req, res, next) {
+  console.log("Posting a users stuff....", req.body)
+  res.render("users/login", {title: "ISS Tracker - Log In"});
+});
+
+router.post("/login", function(req, res, next) {
+  userController.loginUser(req, res, next);
+});
+
 
 module.exports = router;
