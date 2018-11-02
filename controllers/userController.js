a1a446d1cd30e401a98248fdcf13f42f396ad82c
Daniel Masake Kitui <daniel.kitui@andela.com>
2018-11-02 11:01:42 +0300

    Refactor to include custom error messages on validation errors

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,11 +2,11 @@ const User = require("../models/user");
 
 const signupUser = function(req, res, next){
 	if (req.body.password !== req.body.confirmPassword) {
-		res.render('users/signup', {signupError: 'Passwords do not match', data: req.body});
+		res.render('users/signup', {notification: {message: 'Passwords do not match', type: 'alert-danger'}, username: req.body.username, email: req.body.email});
 	}
 	User.create(req.body, function(error, results) {
 		if(error) {
-			res.render('users/signup', {signupError: error, data: req.body});
+			res.render('users/signup', {notification: {message: error, type: 'alert-danger'}, username: req.body.username, email: req.body.email});
 		} else {
 			req.session.email = req.body.email;
 			res.redirect('/users/login');
@@ -18,14 +18,14 @@ const loginUser = function(req, res, next) {
 	if(req.body.email && req.body.password) {
 		User.authenticate(req.body.email, req.body.password, function(error, user) {
 			if (error || !user) {
-				res.render('users/login', {loginError: 'Wrong email or password', useremail: req.body.email});
+				res.render('users/login', {notification: {message: 'Wrong email or password', type: 'alert-danger'}, data: req.body.email});
 			} else {
 				req.session.user = user;
 				res.redirect('/home');
 			}
 		});
 	} else {
-		const error = new Error('Username and/or password is required')
+		const error = new Error('Username and/or password is required');
 		return next(error);
 	}
 };
